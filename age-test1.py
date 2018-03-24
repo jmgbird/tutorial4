8903a8d8ca57692f4e463a97b55a08fb40ef7d7e
James Gilberti <jamesgilberti@jtp4285.frontierlocal.net>
2018-03-24 10:16:06 -0400

    test4

--- age-test1.py
+++ age-test1.py
@@ -5,7 +5,7 @@ if a in range(1, 20):
 	print('You are too young, go home')
 elif a in range(21, 60):
 	print('You are able to drink')
-	print("access key = ASIA9808435848574875")
+	print("access key = ASIA9808435848574876")
 elif a in range(61, 1050):
 	print('You are too old, go home')
 else:
